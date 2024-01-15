3e376f50eb9ef45bddffd9cb67bea84976ee88a1
akop4 <akopov.andrew@gmail.com>
2024-01-15 23:51:55 +0300

    added option --no-fehbg

--- scripts/.config/scripts/feh_random_wallpaper.sh
+++ scripts/.config/scripts/feh_random_wallpaper.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+# !/usr/bin/bash
 # Thanks null user from arch forum
 # https://bbs.archlinux.org/viewtopic.php?pid=1626816#p1626816
 
 while true; do
-  feh --randomize --bg-fill $1/*
+  feh --randomize --no-fehbg --bg-fill $1/*
   sleep $2
 done
