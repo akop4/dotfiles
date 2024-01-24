dae4f7cc7ea87613cacc5047423a73df680a3042
akop4 <akopov.andrew@gmail.com>
2024-01-24 22:55:55 +0300

    added treesitter

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,26 +1,3 @@
--- print("Hello, from init.lua")
--- print("Setting up options")
+require("options")
+require("lazy_plugins")
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
-vim.opt.wrap = false
-
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-
-vim.opt.clipboard = "unnamedplus"
-
--- vim.opt.scrolloff = 999
-
-vim.opt.virtualedit = "block"
-
--- vim.opt.inccommand = "split"
-
-vim.opt.ignorecase = true
-
-vim.opt.termguicolors = true
